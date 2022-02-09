48dac5f7496136b9f1b29eba56ed35a7d499d649
jorddo <jordanbsneed@gmail.com>
2022-02-08 18:17:16 -0600

    writing dynamic weather to current container

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const searchCity = document.querySelector('.txt');
 const searchButton = document.querySelector('.search-btn');
 const date = document.querySelector('.date');
 const currentCity = document.querySelector('#current-city');
+const currentIcon = document.querySelector('#current-icon');
 const currentTemp = document.querySelector('#current-temp');
 const currentWind = document.querySelector('#current-wind');
 const currentHumidity = document.querySelector('#current-humidity');
@@ -12,6 +13,14 @@ const currentDate = moment().format('MMM Do, YYYY');
 
 // write date to page
 date.innerHTML = currentDate;
+
+// enter key submits text area
+searchCity.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchButton.click();
+  }
+});
 
 function currentWeather() {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&appid=${apiKey}&units=imperial
@@ -35,22 +44,20 @@ function currentWeather() {
           console.log(
             `http://openweathermap.org/img/wn/${fiveDayData.daily[0].weather[0].icon}@2x.png`
           );
-          console.log(fiveDayData.daily[0].temp.day);
-          console.log(fiveDayData.daily[0].wind_speed);
-          console.log(fiveDayData.daily[0].humidity);
-          console.log(fiveDayData.daily[0].uvi);
 
           // writing city name to page
           currentCity.textContent = searchCity.value;
 
           // writing current values to current weather box
-
+          currentIcon.src = `http://openweathermap.org/img/wn/${fiveDayData.daily[0].weather[0].icon}@2x.png`;
           currentTemp.textContent = `Temp: ${Math.round(
             fiveDayData.daily[0].temp.day
           )}\u00b0`;
           currentWind.textContent = `Wind: ${fiveDayData.daily[0].wind_speed} MPH`;
           currentHumidity.textContent = `Humidity: ${fiveDayData.daily[0].humidity}`;
           currentUVI.textContent = `UV Index: ${fiveDayData.daily[0].uvi}`;
+
+          // writing values to 5 day forecast cards
         });
     });
 }
